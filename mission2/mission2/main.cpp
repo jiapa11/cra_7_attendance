6c0336c62f0cc236a3d8464c2886666ab89172c9
JH <emlapl@naver.com>
2025-08-21 13:41:54 +0900

    asda

--- mission2/mission2/main.cpp
+++ mission2/mission2/main.cpp
@@ -2,18 +2,22 @@
 #include "gmock/gmock.h"
 #include "attendance_checker.h"
 
+#define UNIT_TEST 1
+
 int main() {
+#if UNIT_TEST
 	::testing::InitGoogleTest();
 	return RUN_ALL_TESTS();
+#else
+	AttendanceChecker checker;
+	try {
+		checker.Run();
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << std::endl;
+		return -1;
+	}
 
-	//AttendanceChecker checker;
-	//try {
-	//	checker.Run();
-	//}
-	//catch (const std::exception& e) {
-	//	std::cout << e.what() << std::endl;
-	//	return -1;
-	//}
-
-	//return 0;	
+	return 0;
+#endif 	
 }
